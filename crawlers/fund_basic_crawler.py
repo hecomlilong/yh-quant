60f6c8ed09afa828b90ef60d2dfa8eab79356ce9
lilong <lilong@360.net>
2019-03-03 21:07:56 +0800

    v0.3 structure init

---
+++ crawlers/fund_basic_crawler.py
@@ -0,0 +1,36 @@
+#  -*- coding: utf-8 -*-
+
+from pymongo import UpdateOne
+from utils.database import DB_CONN
+from utils.tushare_base import TuShareBase
+
+"""
+从tushare获取公募基金数据列表，包括场内和场外基金，保存到本地的MongoDB数据库中
+"""
+
+
+class FundBasicCrawler(TuShareBase):
+    def __init__(self):
+        super(FundBasicCrawler, self).__init__()
+        """
+        初始化
+        """
+
+        # 创建fund数据集
+        self.fund = DB_CONN['fund_basic']
+
+    def crawl(self, market='E'):
+        """
+        抓取公募基金数据列表，包括场内和场外基金
+        :param market: 交易市场: E场内 O场外（默认E）
+        """
+
+        fund_df = self.pro.query('fund_basic', market=market)
+        self.save_data(df=fund_df, collection=self.fund, filter_fields=['ts_code'])
+
+
+# 抓取程序的入口函数
+if __name__ == '__main__':
+    dc = FundBasicCrawler()
+    dc.crawl('E')  # 交易市场: E场内
+    dc.crawl('O')  # 交易市场: O场外
---
+++ crawlers/fund_portfolio_crawler.py
@@ -0,0 +1,44 @@
+#  -*- coding: utf-8 -*-
+
+from utils.database import DB_CONN
+from utils.tushare_base import TuShareBase
+from utils.stock_util import get_all_fund_codes
+from time import sleep
+import traceback
+
+"""
+从tushare获取公募基金持仓数据，保存到本地的MongoDB数据库中
+"""
+
+
+class FundPortfolioCrawler(TuShareBase):
+    def __init__(self):
+        super(FundPortfolioCrawler, self).__init__()
+        """
+        初始化
+        """
+
+        # 创建fund_portfolio数据集
+        self.fund_portfolio = DB_CONN['fund_portfolio']
+
+    def crawl(self):
+        """
+        抓取公募基金持仓数据
+        """
+
+        # 获取基金代码
+        codes = get_all_fund_codes()
+        for code in codes:
+            try:
+                fund_portfolio_df = self.pro.query('fund_portfolio', ts_code=code)
+                self.save_data(df=fund_portfolio_df, collection=self.fund_portfolio,
+                               filter_fields=['ts_code', 'ann_date', 'end_date', 'symbol'])
+                sleep(1)
+            except:
+                print(traceback.print_exc())
+
+
+# 抓取程序的入口函数
+if __name__ == '__main__':
+    dc = FundPortfolioCrawler()
+    dc.crawl()
