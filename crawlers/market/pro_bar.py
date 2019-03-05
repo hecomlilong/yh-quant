dc5a508e7c99318f551408ab1907df34d8662d4e
lilong <lilong@360.net>
2019-03-05 10:00:30 +0800

    pro_bar

--- crawlers/market/pro_bar.py
+++ crawlers/market/pro_bar.py
@@ -11,18 +11,18 @@ class ProBar(TuShareBase):
         """
         初始化
         """
+        # 创建复权数据集
+        self.conn = ''
 
-        # 创建daily数据集
-        # self.conn = DB_CONN['daily']
-
-    def crawl(self, ts_code='', start_date='', end_date='', freq='D', asset='E', adj=None, ma=None):
+    def crawl(self, ts_code='', start_date='', end_date='', freq='D', asset='E', adj='qfq', ma=None):
         """
         :param ts_code: 股票代码
         :param start_date: 开始日期(YYYYMMDD)
         :param end_date: 结束日期(YYYYMMDD)
         :param freq: 数据频度 ：1MIN表示1分钟（1/5/15/30/60分钟） D日线 ，默认D， W周线，M月线
-        :param freq: 数据频度 ：1MIN表示1分钟（1/5/15/30/60分钟） D日线 ，默认D， W周线，M月线
-        :param freq: 数据频度 ：1MIN表示1分钟（1/5/15/30/60分钟） D日线 ，默认D， W周线，M月线
+        :param asset: 资产类别：E股票 I沪深指数 C数字货币 F期货 FD基金 O期权，默认E
+        :param adj: 复权类型(只针对股票)：None未复权 qfq前复权 hfq后复权 , 默认None
+        :param ma: 均线，支持任意周期的均价和均量，输入任意合理int数值
         :return:
         ts_code str 股票代码
         trade_date  str 交易日期
@@ -36,3 +36,15 @@ class ProBar(TuShareBase):
         vol float   成交量 （手）
         amount  float   成交额 （千元）
         """
+        if adj is None:
+            return
+        self.conn = DB_CONN['daily_%s' % adj]
+
+        # 取复权行情
+        df = ts.pro_bar(pro_api=self.pro, ts_code=ts_code, freq=freq, adj=adj, asset=asset, start_date=start_date,
+                        end_date=end_date, ma=ma)
+        self.save_data(df=df, collection=self.conn, filter_fields=['ts_code', 'trade_date'])
+
+if __name__ == '__main__':
+    stock = ProBar()
+    stock.crawl(ts_code='000001.SZ')
