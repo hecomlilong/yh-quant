6db814b686114ab9d4406ec7c9a293a7667fdd53
lilong <lilong@360.net>
2019-03-07 22:59:47 +0800

    atr

--- factors/atr_factor.py
+++ factors/atr_factor.py
@@ -2,10 +2,11 @@
 
 
 from utils.database import DB_CONN
-from utils.stock_util import get_all_codes_pro,get_daily_conn_name
+from utils.stock_util import get_all_codes_pro, get_daily_conn_name
 from pymongo import ASCENDING, UpdateOne
 from pandas import DataFrame
 import traceback
+from utils.tushare_base import TuShareBase
 
 
 def compute_atr(begin_date, end_date):
@@ -20,25 +21,39 @@ def compute_atr(begin_date, end_date):
     # 计算每个股票的信号
     for code in codes:
         try:
-            daily_cursor = DB_CONN[get_daily_conn_name('qfq','E')].find(
-                {'code': code, 'date': {'$gte': begin_date, '$lte': end_date}, 'index': False},
-                sort=[('date', ASCENDING)],
-                projection={'date': True, 'high': True, 'low': True, '_id': False}
+            daily_cursor = DB_CONN[get_daily_conn_name('qfq', 'E')].find(
+                {'ts_code': code, 'trade_date': {'$gte': begin_date, '$lte': end_date}, 'amount': {'$gte': 0},
+                 'index': False},
+                sort=[('trade_date', ASCENDING)],
+                projection={'trade_date': True, 'high': True, 'low': True, 'pre_close': True, '_id': False}
             )
 
             df_daily = DataFrame([daily for daily in daily_cursor])
-            df_daily_shift_1 = df_daily.shift(1)
+
             # 当前交易日的最高价-最低价的绝对值
-            df_daily['current_span'] = df_daily_shift_1['high'] - df_daily_shift_1['low']
+            df_daily['current_span'] = df_daily['high'] - df_daily['low']
 
             # 上一交易日收盘价-当前交易日最低价的绝对值
+            df_daily['close_low'] = (df_daily['pre_close'] - df_daily['low']).abs()
 
             # 上一交易日收盘价-当前交易日最高价的绝对值
+            df_daily['close_high'] = (df_daily['pre_close'] - df_daily['high']).abs()
 
             # 以上三个数的最大值即为ATR
+            df_daily['atr'] = df_daily[['current_span', 'close_low', 'close_high']].max(axis=1)
 
-            # 设置日期作为索引
-            df_daily.set_index(['date'], 1, inplace=True)
+            # 抛掉不用的数据
+            df_daily.drop(['current_span', 'close_low', 'close_high'], 1, inplace=True)
+
+            print(df_daily)
+            # 将信号保存到数据库
+            ts_base = TuShareBase()
+            ts_base.save_data(df=df_daily, collection=DB_CONN[get_daily_conn_name('qfq', 'E')],
+                              filter_fields=['ts_code', 'trade_date'])
         except:
             print('错误发生： %s' % code, flush=True)
             traceback.print_exc()
+
+
+if __name__ == '__main__':
+    compute_atr('2015-01-01', '2015-12-31')
